570710ec9df0c4ee03f06c3b4c275179fb258506
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2023-02-24 23:23:31 -0700

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,37 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
  type User {
     _id: ID!
-    name: String!    
+    name: String! 
+    email: String
+    savedBooks: [Book]!  
+    bookCount: Int 
+ }
+ type Book {
+    bookId: ID!
+    title: String
+    authors: String
+    image: String
+    description: String
+    link: String!
+ }
+
+ type Auth {
+    token: ID!
+    user: User
+    }
+
+ type Query {
+    users: [User]
+    user(userId: ID!): User
+    books: [Books]
+     }
+ 
+ type Mutation {
+    saveBook(book: SaveBookInput): User
+    removeBook(boodId: String!) User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email String!, password: String!): Auth
  }
  `;
+
+ module.exports = typeDefs;
